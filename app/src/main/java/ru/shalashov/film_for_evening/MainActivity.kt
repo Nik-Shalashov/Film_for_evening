6e6997b9567e3891ea56fb5416cb5fc30f155397
Nik-Shalashov <monitor1@inbox.ru>
2021-03-22 23:24:24 +0700

    Проба синтаксиса Kotlin

--- app/src/main/java/ru/shalashov/film_for_evening/MainActivity.kt
+++ app/src/main/java/ru/shalashov/film_for_evening/MainActivity.kt
@@ -1,11 +1,24 @@
 package ru.shalashov.film_for_evening
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        test()
     }
+
+    @SuppressLint("WrongConstant")
+    fun test() {
+        val button: Button = findViewById(R.id.test_button)
+        button.setOnClickListener {
+            Toast.makeText(this, "test", 2).show()
+        }
+    }
+
 }
