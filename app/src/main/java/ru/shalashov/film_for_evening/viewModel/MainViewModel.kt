f0a1ae91a005dc098a09803872c7033324527373
Nik-Shalashov <monitor1@inbox.ru>
2021-03-28 22:53:56 +0700

    Макет ViewModel

---
+++ app/src/main/java/ru/shalashov/film_for_evening/viewModel/MainViewModel.kt
@@ -0,0 +1,21 @@
+package ru.shalashov.film_for_evening.viewModel
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import ru.shalashov.film_for_evening.model.Repository
+import ru.shalashov.film_for_evening.model.RepositoryImpl
+import java.lang.Thread.sleep
+
+class MainViewModel (private val liveDataToObserve: MutableLiveData<AppState> = MutableLiveData(), private val repositoryImpl: Repository = RepositoryImpl()) : ViewModel() {
+
+    fun getLiveData() = liveDataToObserve
+    fun getFilms() = getDataFromLocalSource()
+
+    private fun getDataFromLocalSource() {
+        liveDataToObserve.value = AppState.Loading
+        Thread {
+            sleep(1000)
+            liveDataToObserve.postValue(AppState.Success(repositoryImpl.getFilmFromServer()))
+        }.start()
+    }
+}
