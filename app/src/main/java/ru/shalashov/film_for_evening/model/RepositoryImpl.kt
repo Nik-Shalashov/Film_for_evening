a1d4aa213730e619e6ee606ccb7ccc1b4a9e5001
Nik-Shalashov <monitor1@inbox.ru>
2021-04-01 22:03:26 +0700

    Добавил реализацию новой функции интерфейса Repository

--- app/src/main/java/ru/shalashov/film_for_evening/model/RepositoryImpl.kt
+++ app/src/main/java/ru/shalashov/film_for_evening/model/RepositoryImpl.kt
@@ -4,4 +4,8 @@ class RepositoryImpl: Repository {
     override fun getFilmFromServer(): Film {
         return Film("film", "comedy", "USA", 90, "blabla")
     }
+
+    override fun getAllFilmsFromServer(): List<Film> {
+        return getAllFilms()
+    }
 }
