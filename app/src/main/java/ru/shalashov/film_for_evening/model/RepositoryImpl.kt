ff4b01bf8934eb78e833d5fbde5b287c61140fdf
Nik-Shalashov <monitor1@inbox.ru>
2021-04-08 01:07:06 +0700

    Поправки

--- app/src/main/java/ru/shalashov/film_for_evening/model/RepositoryImpl.kt
+++ app/src/main/java/ru/shalashov/film_for_evening/model/RepositoryImpl.kt
@@ -2,7 +2,7 @@ package ru.shalashov.film_for_evening.model
 
 class RepositoryImpl : Repository {
 
-    override fun getFilmFromServer() = Film("film", "comedy", "USA", 90, "blabla")
+    override fun getFilmFromServer() = Film("film", "comedy", "USA", 90, "blabla", 15)
 
     override fun getAllFilmsFromServer() = getAllFilms()
 }
