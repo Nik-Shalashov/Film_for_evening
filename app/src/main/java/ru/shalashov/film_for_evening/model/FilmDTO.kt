f40ffd98da3d71bb3d441ac6e98437ab3d356a7a
Nik-Shalashov <monitor1@inbox.ru>
2021-04-08 01:09:01 +0700

    Поправил класс в соответствии с API

--- app/src/main/java/ru/shalashov/film_for_evening/model/FilmDTO.kt
+++ app/src/main/java/ru/shalashov/film_for_evening/model/FilmDTO.kt
@@ -3,9 +3,19 @@ package ru.shalashov.film_for_evening.model
 data class FilmDTO(
     val id: Int,
     val title: String,
-    val genres: Map<Int, String>,
+    val genres: List<Genres>,
     val overview: String?,
     val runtime: Int?,
-    val production_countries: List<String>,
+    val production_countries: List<Production_countries>,
     val vote_average: Float?
+)
+
+data class Genres(
+    val id: Int,
+    val name: String
+)
+
+data class Production_countries(
+    val iso_3166_1: String,
+    val name: String
 )
